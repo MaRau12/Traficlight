f7291b1b6c25391accd5d3ded491a10836036114
MaRau12 <marthaelisabeth94@gmail.com>
2023-02-23 20:30:11 +0000

    basic structure

---
+++ src/js/component/lights.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+ export const Lights = (props) =>{
+    return ( <div>
+           <div className="red"></div>
+           <div className="yellow"></div>
+           <div className="green"></div>
+         </div> )
+        
+}
+;
+
