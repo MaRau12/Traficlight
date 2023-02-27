9d54814788129ffbfcf5983bfca3269612ffebc1
MaRau12 <marthaelisabeth94@gmail.com>
2023-02-27 16:16:32 +0000

    finished

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React , { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
-import { Lights } from "./lights.jsx";
+
+
 //create your first component
 const Home = () => {
+	const [selectedColor, setselectedColor] = useState("red");
 	return (
 		<div className="box">
-         <Lights/>
+        <div onClick={()=> setselectedColor("red")}
+		    className={"light red" + (selectedColor === "red" ? " glow" : "")}></div>
+		<div onClick={()=> setselectedColor("yellow")}	
+		     className={"light yellow" + (selectedColor === "yellow" ? " glow" : "")}></div>
+		<div onClick={()=> setselectedColor("green")}	
+		     className={"light green" + (selectedColor === "green" ? " glow" : "")}></div>
 		</div>
+		
 	);
 };
 
